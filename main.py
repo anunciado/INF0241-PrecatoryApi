e1b74572511d7243ecc89755d0744b301f4a3f4e
Luis Eduardo Anunciado Silva <t_luissilva@jfrn.jus.br>
2025-02-11 21:26:41 -0300

    feat: primeira versão da api

---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="API da Aula 3",
+    description=description,
+    version="0.1",
+    terms_of_service="http://example.com/terms/",
+    contact={
+        "name": "Rogério Rodrigues Carvalho",
+        "url": "http://github.com/rogerior/",
+        "email": "rogerior@ufg.br",
+    },
+    license_info={
+        "name": "Apache 2.0",
+        "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
+    },
+    dependencies=[Depends(commom_verificacao_api_token)],
+)
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
